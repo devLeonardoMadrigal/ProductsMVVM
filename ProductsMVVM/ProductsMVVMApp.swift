67dbc59fb335aab24654dbec0aa7c806445c259c
Leonardo Madrigal <DevLeonardoDavid@gmail.com>
2026-03-23 14:49:38 -0400

    Initial Commit

---
+++ ProductsMVVM/ProductsMVVMApp.swift
@@ -0,0 +1,21 @@
+//
+//  ProductsMVVMApp.swift
+//  ProductsMVVM
+//
+//  Created by Leonardo Madrigal on 3/23/26.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct ProductsMVVMApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ ProductsMVVMTests/ProductsMVVMTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProductsMVVMTests.swift
+//  ProductsMVVMTests
+//
+//  Created by Leonardo Madrigal on 3/23/26.
+//
+
+import Testing
+@testable import ProductsMVVM
+
+struct ProductsMVVMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
